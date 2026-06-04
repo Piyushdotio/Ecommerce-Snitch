44ae34c7fbc39bf2713cffadb3135bd8f1aa709d
Piyush-dross <kumarsinghpiyush62@gmail.com>
2026-06-04 21:14:16 +0530

    Refactor code structure for improved readability and maintainability

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -16,6 +16,12 @@ if(!process.env.GOOGLE_CLIENT_SECRET){
 if(!process.env.PRIVATE_KEY){
     throw new Error("PRIVATE_KEY is not defined in environmental variables")
 }
+if(!process.env.RAZORPAY_KEY_ID){
+    throw new Error("RAZORPAY_KEY_ID is not defined in environmental variables")
+}
+if(!process.env.RAZORPAY_KEY_SECRET){
+    throw new Error("RAZORPAY_KEY_SECRET is not defined in environmental variables")
+}
 
 export const config={
     MONGO_URI:process.env.MONGO_URI,
@@ -26,7 +32,8 @@ export const config={
     PRIVATE_KEY:process.env.PRIVATE_KEY,
     CLIENT_URL:process.env.CLIENT_URL || "http://localhost:5173",
     SERVER_URL:process.env.SERVER_URL || "http://localhost:3000",
-    GOOGLE_CALLBACK_URL:
-        process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/api/auth/google/callback",
+    GOOGLE_CALLBACK_URL:process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/api/auth/google/callback",
+    RAZORPAY_KEY_ID:process.env.RAZORPAY_KEY_ID,
+    RAZORPAY_KEY_SECRET:process.env.RAZORPAY_KEY_SECRET
 
 }
